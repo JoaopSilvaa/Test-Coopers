2b7f0db9b9a8ece7e58bbbb38441792301e3e2cb
Joao Antonio <joaopsilva.dev@gmail.com>
2023-01-25 17:34:45 -0300

    feat: :construction: iniciando construção de estrutura do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import fill from '../assets/Fill1.png';
+import room from '../assets/Room.jpg';
 
 export default function Header() {
   return (
     <section>
-      <h1>Olá</h1>
+      <header>
+        <div>
+          <img src={ fill } alt="logo coopers" />
+          <span>coopers</span>
+        </div>
+        <button
+          type="button"
+        >
+          entrar
+        </button>
+      </header>
+      <div>
+        <div>
+          <p>
+            Organize
+            <br />
+            <span>your daily jobs</span>
+          </p>
+          <p>The only way to get things done</p>
+          <span>Go to to-do list</span>
+        </div>
+        <img src={ room } alt="sala comum ambiente agradável" />
+      </div>
     </section>
   );
 }
