651fb752ec4ec01b1e74d1b990c66514c70bd749
Joao Antonio <joaopsilva.dev@gmail.com>
2023-01-30 19:48:20 -0300

    style: :lipstick: estilização para mobile completa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Footer.css';
+import bg from '../assets/BGFooter.png';
+import bgGreen from '../assets/BGreen.png';
 
 export default function Footer() {
   return (
     <footer className="Footer">
-      <span>Need help?</span>
-      <span>coopers@coopers.pro</span>
-      <span>© 2021 Coopers. All rights reserved.</span>
+      <img src={ bg } id="bgFooter" alt="imagem de fundo listra preta" />
+      <div className="textFooter">
+        <span id="need">Need help?</span>
+        <span id="emailFooter">
+          coopers@coopers.pro
+          <br />
+        </span>
+        <span id="reserv">©2021 Coopers. All rights reserved.</span>
+      </div>
+      <img src={ bgGreen } id="bgFooter2" alt="imagem de fundo verde" />
     </footer>
   );
 }
